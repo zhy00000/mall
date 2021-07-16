0707d621322894e96c8d78114a14416fd8f2b9dc
zhy00000 <84761186+zhy00000@users.noreply.github.com>
2021-07-16 23:55:28 +0800

    提交信息

---
+++ src/views/home/Home.vue
@@ -0,0 +1,145 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  <home-swiper :banners="banners"></home-swiper>
+  <recommend-view :recommends="recommends"></recommend-view>
+  <feature-view></feature-view>
+  <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
+  <goods-list :goods="goods['pop'].list"></goods-list>
+
+  <ul>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+        <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+        <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+        <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+    <li>列表</li>
+  </ul>
+  </div>
+</template>
+
+<script>
+
+import HomeSwiper from './childComps/HomeSwiper.vue'
+import RecommendView from './childComps/RecommendView.vue'
+import FeatureView from './childComps/FeatureView.vue'
+
+import NavBar from '@/components/common/navbar/NavBar'
+import TabControl from '@/components/content/tabControl/TabControl'
+import GoodsList from '@/components/content/goods/GoodsList.vue'
+
+
+import {getHomeMultidata,getHomeGoods} from '@/network/home.js'
+
+export default {
+    name:'Home',
+    components:{
+      
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+      NavBar,
+      TabControl,
+      GoodsList,
+    },
+    data(){
+      return {
+        banners:[],
+        recommends:[],
+        goods:{
+          'pop':{page:0,list:[]},
+          'new':{page:0,list:[]},
+          'sell':{page:0,list:[]},
+        }
+      }
+    },
+    created(){
+      // 1.请求多个数据
+      this.getHomeMultidata()
+
+      // 2.请求商品数据
+      this.getHomeGoods('pop')
+
+      this.getHomeGoods('new')
+
+      this.getHomeGoods('sell')
+    },
+    methods:{
+      getHomeMultidata(){
+          getHomeMultidata().then(res=>{
+        // console.log(res);
+        // this.result = res
+            this.banners=res.data.banner.list;
+            this.recommends=res.data.recommend.list;
+      })
+      },
+      getHomeGoods(type){
+          const page=this.goods[type].page+1
+          getHomeGoods(type,page).then(res=>{
+            this.goods[type].list.push(...res.data.list)
+            this.goods[type].page+=1
+      })
+      }
+    }
+}
+
+</script>
+
+<style>
+  #home {
+    padding-top: 44px;
+  }
+  .home-nav{
+    background-color: var(--color-tint);
+    color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+
+  .tab-control{
+    /* position: sticky; 基于用户的滚动位置来定位 */
+    /* 粘性定位的元素是依赖于用户的滚动，在 position:relative 与 position:fixed 定位之间切换。 */
+    position: sticky;
+    top: 44px;
+  }
+</style>
